82298c95554c8d5221f7113d308a060b111b7558
krufusraj <78624709+krufusraj@users.noreply.github.com>
2023-10-17 15:51:55 -0400

    working before review

---
+++ backend/index.js
@@ -0,0 +1,32 @@
+import app from "./server.js"
+import mongodb from "mongodb"
+import dotenv from "dotenv"
+import MoviesDAO from "./dao/moviesDAO.js"
+
+async function main(){
+
+    dotenv.config()
+
+    const client = new mongodb.MongoClient(
+        process.env.MOVIEREVIEWS_DB_URI
+    )
+
+    const port = process.env.PORT || 8000
+
+    try{
+
+        //conncet to the MngoDV cluster
+        await client.connect()
+        await MoviesDAO.injectDB(client)
+
+        app.listen(port, () =>{
+            console.log('server is running on port:'+port);
+        })
+    }
+    catch(e){
+        console.error(e);
+        process.exit(1)
+    }
+}
+
+main().catch(console.error);
